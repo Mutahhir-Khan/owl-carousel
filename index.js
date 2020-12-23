71cb990d865847aee3541102b790e2698b98dc16
Mutahhir-Khan <63418479+Mutahhir-Khan@users.noreply.github.com>
2020-12-24 02:23:06 +0500

    all set, just remove navText

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 500,
   nav: true,
+  // navText:["",""],
   autoplay: true,
   smartSpeed: 600,
   autoplayTimeout: 10000,
@@ -15,8 +16,31 @@ $(".owl-carousel").owlCarousel({
     1100: { items: 1 },
   },
   animateIn: 'fadeIn',
-  animateOut: 'fadeOut'
+  animateOut: 'fadeOut',
 });
+
+var navTextChanger = (innerWidth) => {
+  if(innerWidth < 768) {
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 500,
+      nav: true,
+      navText:[" "," "],
+      autoplay: true,
+      smartSpeed: 600,
+      autoplayTimeout: 10000,
+      autoHeight: true,
+      responsive: {
+        0: { items: 1 },
+        600: { items: 1 },
+        1100: { items: 1 },
+      },
+      animateIn: 'fadeIn',
+      animateOut: 'fadeOut',
+    });
+    console.log($(".owl-carousel").owlCarousel())
+  }
+}
 
 var owl = $('.owl-carousel');
 owl.owlCarousel();
@@ -24,6 +48,7 @@ owl.owlCarousel();
 $('.clickable').click(function() {
     owl.trigger('next.owl.carousel');
 })
+
 // Go to the previous item
 // $('.customPrevBtn').click(function() {
 //     // With optional speed parameter
@@ -36,6 +61,7 @@ var carouselImagesChanger = () => {
   if (window.innerWidth < 768) {
     desktopContainer.style.display = "none";
     mobileContainer.style.display = "block"
+    navTextChanger(window.innerWidth)
   } else {
     desktopContainer.style.display = "block";  
     mobileContainer.style.display = "none"
@@ -44,3 +70,14 @@ var carouselImagesChanger = () => {
 
 window.addEventListener("resize", carouselImagesChanger);
 window.onload = () => carouselImagesChanger();
+
+// LOADER -----------------------------------------------
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loader");
+  const loaderImg = document.querySelector(".loader img");
+  loader.className += " hidden"; // class "loader hidden"
+    loader.style.height = 0
+    loader.style.width = 0
+    loaderImg.style.width = 0
+    loaderImg.style.height = 0
+});
